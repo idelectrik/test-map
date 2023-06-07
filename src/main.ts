c7c18d15b42641f1a0f56c71d0e832827e79f705
idelectrik <idelectrik@gmail.com>
2023-06-07 12:03:36 +0200

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -12,13 +12,62 @@ WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+    WA.room.area.onEnter('indice1').subscribe(() => {
+        // const today = new Date();
+        // const time = today.getHours() + ":" + today.getMinutes();
+        currentPopup = WA.ui.openPopup("indice1Popup", "C'est l'indice 1", []);
     })
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+    WA.room.area.onLeave('indice1').subscribe(closePopup)
+
+    WA.room.area.onEnter('indice2').subscribe(() => {
+        currentPopup = WA.ui.openPopup("indice2Popup", "C'est l'indice 2", []);
+    })
+
+    WA.room.area.onLeave('indice2').subscribe(closePopup)
+
+    WA.room.area.onEnter('indice3').subscribe(() => {
+        currentPopup = WA.ui.openPopup("indice3Popup", "C'est l'indice 3", []);
+    })
+
+    WA.room.area.onLeave('indice3').subscribe(closePopup)
+
+    WA.room.area.onEnter('indice4').subscribe(() => {
+        currentPopup = WA.ui.openPopup("indice4Popup", "C'est l'indice 4", []);
+    })
+
+    WA.room.area.onLeave('indice4').subscribe(closePopup)
+
+    WA.room.area.onEnter('indice5').subscribe(() => {
+        currentPopup = WA.ui.openPopup("indice5Popup", "C'est l'indice 5", []);
+    })
+
+    WA.room.area.onLeave('indice5').subscribe(closePopup)
+
+    WA.room.area.onEnter('indice6').subscribe(() => {
+        currentPopup = WA.ui.openPopup("indice6Popup", "C'est l'indice 6", []);
+    })
+
+    WA.room.area.onLeave('indice6').subscribe(closePopup)
+
+    WA.room.area.onEnter('indice7').subscribe(() => {
+        currentPopup = WA.ui.openPopup("indice7Popup", "C'est l'indice 7", []);
+    })
+
+    WA.room.area.onLeave('indice7').subscribe(closePopup)
+
+    WA.room.area.onEnter('indice8').subscribe(() => {
+        currentPopup = WA.ui.openPopup("indice8Popup", "C'est l'indice 8", []);
+    })
+
+    WA.room.area.onLeave('indice8').subscribe(closePopup)
+
+    WA.room.area.onEnter('indice9').subscribe(() => {
+        currentPopup = WA.ui.openPopup("indice9Popup", "C'est l'indice 9", []);
+    })
+
+    WA.room.area.onLeave('indice9').subscribe(closePopup)
+
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
